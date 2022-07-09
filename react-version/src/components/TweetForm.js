a830e721783603d097661e75a80a855c8cdc29ae
AVacAdok <avakuang8@gmail.com>
2022-07-09 02:55:36 +0000

    converting HTML content

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -1,8 +1,16 @@
 import React from 'react'; //optional
 
 function TweetForm(){
+  const placeholder = "What are you humming about?";
+
   return (
-    <h1>I am the tweet form component</h1>
+    <section className="newtweet">
+      <form method="post" action="/tweets" className="newtweet__form">
+        <textarea className="form__textarea" name="text" placeholder={placeholder}></textarea>
+        <input type="submit" value="Tweet" className="form__input" />
+        <span className="form__counter">140</span>
+      </form>
+    </section>
   );
 }
 
--- react-version/src/components/TweetList.js
+++ react-version/src/components/TweetList.js
@@ -1,8 +1,52 @@
 import React from 'react'; //optional
+import Tweet from './Tweet';
 
 function TweetList(){
+
   return (
-    <h1>I am the tweet list component</h1>
+    <section className="tweets">
+    <article className="tweet">
+      <header className="tweet--header">
+        <img className="tweet--avatar" src={Tweet.avatar} />
+        <h2 className="tweet--name">{Tweet.lastName}</h2>
+        <small className="tweet--handle">{Tweet.handle}</small>
+      </header>
+
+      <div className="tweet--body">
+        <p>{Tweet.body}</p>
+      </div>
+
+      <footer className="tweet--footer">
+        <small className="footer--age">{Tweet.age}<small>
+            <span className="footer--actions">
+              <a href="#"><i className="fa fa-flag"></i></a>
+              <a href="#"><i className="fa fa-retweet"></i></a>
+              <a href="#"><i className="fa fa-heart"></i></a>'
+            </span>
+          </small></small></footer>
+    </article>
+    <article className="tweet">
+      <header className="tweet--header">
+        <img className="tweet--avatar" src={Tweet.avatar} />
+        <h2 className="tweet--name">{Tweet.lastName}</h2>
+        <small className="tweet--handle">{Tweet.handle}</small>
+      </header>
+
+      <div className="tweet--body">
+        <p>{Tweet.body}</p>
+      </div>
+
+      <footer className="tweet--footer">
+        <small className="footer--age">{Tweet.age}<small>
+            <span className="footer--actions">
+              <a href="#"><i className="fa fa-flag"></i></a>
+              <a href="#"><i className="fa fa-retweet"></i></a>
+              <a href="#"><i className="fa fa-heart"></i></a>'
+            </span>
+          </small></small></footer>
+    </article>
+
+  </section>
   );
 }
 
